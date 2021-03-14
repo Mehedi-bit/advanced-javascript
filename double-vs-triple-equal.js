bfbdcd3293c284f177fa84b63da133c22697f85b
Md. Mehedi Hasan Emon <60240261+Mehedi-bit@users.noreply.github.com>
2021-03-14 17:05:07 +0600

    map added

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,27 @@
+const first = 2;
+const second = "2";
+if (first == second) {
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+const third = 2;
+const fourth = "2";
+if (first === second) {
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+
+const x = 1;
+const y = true;
+if (first == second) {
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
