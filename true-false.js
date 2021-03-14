b7ec8363353c29bf18794fc504bb7bd86c159f81
Md. Mehedi Hasan Emon <60240261+Mehedi-bit@users.noreply.github.com>
2021-03-14 10:09:03 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,16 @@
+// .........falsy.........
+// 0
+// ""
+// undefined variable  (ex. let x;)
+// null
+// NaN
+
+// ..........truthy..........
+// 12
+// -4
+// "0"
+// " "
+// "anything"
+// defined variable  (ex. let x = 5;)
+// {}
+// []
